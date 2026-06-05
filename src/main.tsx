43d4923fae4456f83882a8380ba772bfe1486337
Roy Lu <Roylu0404@gmail.com>
2026-06-05 10:08:46 -0400

    chore: Important file updates
    forgot to commit, oopsies!

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./styles/theme.css";
+import "./styles/utilities.css";
+import "./styles/layout.css";
+import "./styles/downloads.css";
+import "./styles/settings.css";
+import "./styles/feedback.css";
+import "./styles/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
